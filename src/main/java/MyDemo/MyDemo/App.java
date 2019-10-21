c3e116b7a3694698b03d198e9d75452eec96ae3a
Manoj Yadav <manojkumar.yadav@kantar.com>
2019-10-21 22:42:35 +0530

    My first maven project commit

---
+++ src/main/java/MyDemo/MyDemo/App.java
@@ -0,0 +1,13 @@
+package MyDemo.MyDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
